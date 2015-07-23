27e28e34619c08257843dbc002d14b612ae74ac0
pnitto <pnitto18@gmail.com>
2015-07-22 21:43:27 -0400

    Added some separators and validators

--- textminer/separator.py
+++ textminer/separator.py
@@ -1,16 +1,40 @@
 import re
 
 
-def words(input):
-    word = r'\w+\d*\w'
-    return re.findall(word,input)
-##might need re.split()
+def words(w):
+    if len(re.findall(r"[a-zA-Z]", w)) > 0:
+        return w.split()
 
-#def phone_numbers(input):
- #   phone_number = r'(?:\(?(\d{3})\)?[\-\.]?\s*)?(\d{3})[\-\.]?(\d{4})'
- #   print({"area_code" : " ", "number" : " "})
- #   return re.findall(phone_number, input)
+def phone_number(v):
+    return re.fullmatch(r'[\d({3})\d{3}\d{4}]', v)
 
 
-def money(input):
-    money_1 =
+def money(x):
+    if len(x) < 2:
+        return None
+    if re.fullmatch(r"\$.*\.\d{1}|\$.*\.\d{3}|\${2}\d.*|\$\d*,\d{1,2}|\d+", x):
+        return None
+    else:
+        cleantext = ''.join((re.findall(r"[\d\.]", x)))
+        print(cleantext)
+        return{"currency": "$", "amount": float(cleantext)}
+
+
+def zip_code(address):
+       if len(re.findall(r"[\d]", address)) == 9 or len(re.findall(r"[\d]", address)) == 5:
+        cleantext = ''.join((re.findall(r"[\d]", address)))
+        print(cleantext)
+        zip = re.findall(r"^\d{5}", cleantext)
+        number = cleantext[5:]
+        if number == '':
+            number = None
+        else:
+            number = ''.join(number)
+        return {"zip": ''.join(zip), "plus4": number}
+
+def date(d):
+    date = re.search(r'(\d+/\d+/\d+)', d)
+    return date
+
+def emails(w):
+     return re.findall(r"[a-z\.]*\@[a-z\.]*", w)
--- textminer/validator.py
+++ textminer/validator.py
@@ -1,25 +1,56 @@
 import re
 
 def binary(v):
-    one_or_zero = r'[0-1]'
-    if re.match(one_or_zero,v):
+    regr = re.findall(r'\b[01]+\b',v)
+    if len(v) == 0:
+        return False
+    if len(regr) > 0:
         return True
-    
+
 def binary_even(v):
     even_binary = r'[0-1]+0$'
     if re.match(even_binary,v):
         return True
 
 
-def hex(v):
-    hex_number = r'[^\da-fA-F]+'
-    if re.findall(hex_number, v):
+def hex(hexnum):
+  answer = bool
+  express = re.findall(r"[a-fA-F0-9]", hexnum)
+  original = [_ for _ in hexnum]
+  new = [_ for _ in express]
+  if len(hexnum) == 0:
+      answer = False
+  else:
+      answer = len(original) == len(new)
+  return answer
+
+def word(single):
+    return re.search(r'^[A-Za-z\d/-]+[A-Za-z]$', single)
+
+def phone_number(number):
+  answer = bool
+  new = re.findall(r'[\d{3}\d{3}\d{4}]', number)
+  print(new)
+  if len(new) == 10:
+      answer = True
+  else:
+      answer = False
+  return answer
+
+def zipcode(z):
+    postal_code = re.match('^\d{5}(-\d{4})?$', z)
+    return postal_code
+
+def money(y):
+    if len(y) < 2:
+        return False
+    if re.fullmatch(r"\$.*\.\d{1}|\$.*\.\d{3}|\${2}\d.*|\$\d*,\d{1,2}|\d+", y):
+        return False
+    else:
         return True
 
-    ##could not get this one!
-def word(v):
-    words = r'[0-9--a-z]+'
-    if re.findall(words,v):
-        return True
-
+def words(x):
     pass
+
+def date(dates):
+    return re.search(r'^(\d{1,4}[/-]){2}\d{1,4}$', dates)
